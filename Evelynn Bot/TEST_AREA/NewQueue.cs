0293156ca274de6a8132d13d75353248ca297ee3
MURAT ULAŞ ÖZTÜRK <ozturk18118@yazilimlisesi.k12.tr>
2021-10-01 11:50:14 +0300

    Randomize Champion Selects + 3.0.1.4

--- Evelynn Bot/TEST_AREA/NewQueue.cs
+++ Evelynn Bot/TEST_AREA/NewQueue.cs
@@ -94,18 +94,34 @@ namespace Evelynn_Bot
                 itsInterface2.logger.Log(false,itsInterface2.messages.ErrorCreateGame + e);
             }
         }
+        public static List<T> Shuffle<T>(List<T> list)
+        {
+            var rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            return list;
+        }
 
         async void PickChampion()
         {
             try
             {
                 List<long> pickableChampions = await itsInterface2.lcuPlugins.GetPickableChampions();
+                pickableChampions = Shuffle<long>(pickableChampions);
 
                 LolChampSelectChampSelectSession champSession = await itsInterface2.lcuPlugins.GetChampSelectSessionAsync();
                 long localPlayerCellId = champSession.localPlayerCellId.Value;
                 long value2 = champSession.actions[0][(int)localPlayerCellId].id.Value;
                 long selectedChampId = -1;
                 bool isLocked = false;
+                itsInterface2.championDatas.ADCChampions = Shuffle<long>(itsInterface2.championDatas.ADCChampions);
                 foreach (long championId in pickableChampions.Where((long cId) => itsInterface2.championDatas.ADCChampions.Contains(cId)))
                 {
                     itsInterface2.lcuPlugins.SelectChampionAsync(value2, championId, localPlayerCellId);
